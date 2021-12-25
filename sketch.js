6832aa8cd4747c60b33b47ff9c78a78073f6f122
Ana <anna.lesi2000@gmail.com>
2021-12-25 13:58:03 +0200

    updated player moves; added shadow

--- sketch.js
+++ sketch.js
@@ -1,218 +1,160 @@
-var stage = 0;
+var player1 = [];
+var player2 = [];
+var imagesPath = 'assets/images/'
 
-//Player1 assets
-var player1X = 400;
-var player1Y = 375;
-var player1Width = 128;
-var player1Height = 156;
-
-var jumpPlayer1 = false; //are we jumping?
-var directionPlayer1 = 1; //the force of gravity in the y direction
-var velocityPlayer1 = 2; //the speed of player
-var jumpPowerPlayer1 = 15; //the energy or strength of player
-var fallingSpeedPlayer1 = 5; //equal to velocity
-var jumpCounterPlayer1 = 0; //keeps track of how much we are jumping
-
-//Player1 assets
-var player2X = 200;
-var player2Y = 546;
-var player2Width = 128;
-var player2Height = 156;
-
-var jumpPlayer2 = false; //are we jumping?
-var directionPlayer2 = 1; //the force of gravity in the y direction
-var velocityPlayer2 = 2; //the speed of player
-var jumpPowerPlayer2 = 15; //the energy or strength of player
-var fallingSpeedPlayer2 = 5; //equal to velocity
-var jumpCounterPlayer2 = 0; //keeps track of how much we are jumping
-
-var minHeight = 546; //height of ground
-var maxHeight = 50; //height of sky
-
-//boxes (platforms)
-var b1X = 1000; //b1 for box 1
-var b1Y = 400;
-var bWidth = 200;
-var bHeight = 80;
-
-//media
-var player1;
-var player1Flipped;
-var player2;
-var player2Flipped;
-var platform;
-var landscape;
-var jumpSound;
+function preload() {
+}
 
 function setup() {
-  createCanvas(1104, 624);
-  rectMode(CENTER);
-	textAlign(CENTER);
-	imageMode(CENTER);
+  createCanvas(800, 800);
+  player1[0] = initializePlayer(200, 200, 64, 64, 'player1')
+  player1[1] = 'D'
+  player1[2] = addShadow(player1)
+  //player2 = initializePlayer(300, 300, 64, 64, 'player2')
 }
 
 function draw() {
-
-  player1Move();
-  player2Move();
-	gravity();
-
-  if(stage == 0){
-    startScreen();
-  }
-
-  if(stage == 1){
-		game();
-	}//close = 1
-	
-	if(mouseIsPressed == true){
-		stage = 1;
-	}//click starts game
-
-}
-
-function startScreen(){
   background(220);
-  image(landscape, width/2, height/2, width, height);
-  fill(255);
-  stroke(0);
-  strokeWeight(10);
-  textSize(75);
-  text('PEEPEE & POOPOO', width/2, 180);
-  textSize(50);
-  text('Adventure Time', width/2, 270);
+  drawSprite(player1[2])
+  drawSprite(player1[0])
+  playerMove(player1, 'W', 'S', 'A', 'D', 'Q', 'E')
+  //drawSprite(player2)
+  //playerMove(player2, 'UP_ARROW', 'DOWN_ARROW', 'LEFT_ARROW', 'RIGHT_ARROW', 'N', 'M', 'L')
 }
 
-function preload() {
-  player1 = loadImage('assets/images/FVikingIdle.png')
-  player1_img = loadImage('assets/images/FVikingIdle.png')
-  player1Flipped = loadImage('assets/images/FVikingIdleFlipped.png')
-  player2 = loadImage('assets/images/VikingIdle.png')
-  landscape = loadImage('assets/images/snowland.png')
+function initializePlayer(x, y, height, width, playerName) {
+  var player = createSprite(x, y, height, width)
+  path = imagesPath + playerName
+  downPath = path + '/'
+  addAnimations(player, playerName, 'D', 'idleD', 1);
+  addAnimations(player, playerName, 'D', 'walkD', 6);
+  addAnimations(player, playerName, 'D', 'attackD', 9);
+  addAnimations(player, playerName, 'RL', 'idleRL', 1);
+  addAnimations(player, playerName, 'RL', 'walkRL', 6);
+  addAnimations(player, playerName, 'RL', 'attackRL', 9);
+  addAnimations(player, playerName, 'U', 'idleU', 1);
+  addAnimations(player, playerName, 'U', 'walkU', 6);
+  addAnimations(player, playerName, 'U', 'attackU', 9);
+  player.velocity.y = 0
+  return player
 }
 
-
-function game(){
-  //appearance of game
-    background(220);
-    image(landscape, width/2, height/2, width, height);
-  //window frame
-    noFill();
-    stroke(0);
-    strokeWeight(15);
-    rect(width/2, height/2, width, height);
-  
-  //draw box
-	stroke(0);
-	strokeWeight(5);
-	fill(255, 120, 0);//orange
-	rect(b1X, b1Y, bWidth, bHeight);
-
-  //collisions with boxes
-	if(player1X >= b1X-bWidth/2 && player1X <= b1X+bWidth/2 && player1Y+player1Height/2 >= b1Y-bHeight/2 && player1Y-player1Height/2 <= b1Y+bHeight/2 && jumpPlayer1 == false){
-		player1Y = b1Y-55;//dont fall and rest on top of platform
-		velocityPlayer1 = 0; //no speed becuase we arent falling
-		jumpCounterPlayer1 = 0;//allows us to jump again
-	}//close if on box
-
-
-  //draw player
-    stroke(0);
-    strokeWeight(5);
-    fill(150, 0, 170);//purple
-    image(player1, player1X, player1Y, player1Width, player1Height);
-
-  //draw player
-  stroke(0);
-  strokeWeight(5);
-  fill(150, 0, 170);//purple
-  image(player2, player2X, player2Y, player2Width, player2Height);
-    
-  }//close game
-
-
-///////////////////////////////////////////gravity
-function gravity(){
-
-	if(player1Y >= minHeight && jumpPlayer1 == false) {
-		//STOP FALLING ON THE GROUND
-		player1Y = player1Y; //dont fall
-		jumpCounterPlayer1 = 0;//reset jump counter when landing
-	}//close on ground
-	else {
-		player1Y = player1Y + (directionPlayer1*velocityPlayer1); //the code that makes gravity work
-	}//else fall
-	
-	
-	if(jumpPlayer1 == true){
-		if(player1Y <= maxHeight || jumpCounterPlayer1 >= jumpPowerPlayer1){
-			if(player1Y >= minHeight){
-				player1Y = minHeight;
-			}//close at min already
-			else{
-				velocityPlayer1 = fallingSpeedPlayer1; //fall at maximums
-			}//close else not at min
-		}//close at max
-		else {
-			velocityPlayer1 = -jumpPowerPlayer1; //jumping
-			jumpCounterPlayer1 = jumpCounterPlayer1 + 1;//add to jump counter
-		}//close else not at max
-	}//close jump
-	else{
-		velocityPlayer1 = fallingSpeedPlayer1;
-	}//close not jumping
-	
-}//close gravity
-
-function player1Move(){
-  if (keyDown('LEFT_ARROW')) {
-		player1X = player1X - 5; //move left
-	}//close left
-	
-	if (keyDown('RIGHT_ARROW')) {
-		player1X = player1X + 5; //move right
-    push();
-    image(player1Flipped, player1X, player1Y, player1Width, player1Height);
-    pop();
-	}//close right
-
-  if (keyDown('DOWN_ARROW')) {
-    player1Y = player1Y + 5;
+function addAnimations(playerSprite, playerName, direction, animationName, numberOfFrames){
+  path = imagesPath + `${playerName}/${direction}/${animationName}_`;
+  var framePaths = [];
+  for (let i = 0; i < numberOfFrames; i++) {
+    framePaths[i] = path + i + '.png'
   }
+  animation = loadAnimation.apply(this, framePaths);
+  playerSprite.addAnimation(animationName, animation)
+}
 
-  if (keyDown('UP_ARROW')) {
-    player1Y = player1Y - 5;
-  }
+function addShadow(player) {
+  var shadow = createSprite(player[0].position.x, player[0].position.y + 38, 150, 160)
+  shadow.addImage(loadImage(`${imagesPath}/shadow.png`))
+  shadow.scale = 0.4
+  player[0].collide(shadow)
+  return shadow
+}
 
-  if (keyDown('SPACE')) {
-    jumpPlayer1 = true;
+function playerMove(player, moveUpKey, moveDownKey, moveLeftKey, moveRightKey, attackKey, jumpKey) {
+  var moveSpeed = 5
+  var playerSprite = player[0]
+  var shadow = player[2]
+
+  if (keyDown(jumpKey)) {
+    playerSprite.scale = 2.3
+    shadow.scale = 0.45
+    playerSprite.position.y = shadow.position.y - 58
   } else {
-    jumpPlayer1 = false;
+    playerSprite.scale = 2
+    shadow.scale = 0.4
+    playerSprite.position.y = shadow.position.y - 38
+  }
+
+  if (keyDown(moveUpKey)) {
+    playerSprite.changeAnimation('walkU')
+    playerSprite.position.y -= moveSpeed
+    shadow.position.y -= moveSpeed
+    player[1] = 'U'
+  }
+  else if (keyDown(moveDownKey)) {
+    playerSprite.changeAnimation('walkD')
+    playerSprite.position.y += moveSpeed
+    shadow.position.y += moveSpeed
+    player[1] = 'D'
+  }
+  else if (keyDown(moveLeftKey)) {
+    playerSprite.changeAnimation('walkRL')
+    playerSprite.mirrorX(-1)
+    playerSprite.position.x -= moveSpeed
+    shadow.mirrorX(-1)
+    shadow.position.x -= moveSpeed
+    player[1] = 'R'
+  }
+  else if (keyDown(moveRightKey)) {
+    playerSprite.changeAnimation('walkRL')
+    playerSprite.mirrorX(1)
+    playerSprite.position.x += moveSpeed
+    shadow.mirrorX(1)
+    shadow.position.x += moveSpeed
+    player[1] = 'L'
+  } else {
+    switchIdle(player)
+  }
+
+  if (keyDown(attackKey)) {
+    switchAttack(player)
   }
 }
 
-function player2Move(){
-  if (keyDown('A')) {
-		player2X = player2X - 5; //move left
-    image(player2, player2X, player2Y, player2Width, player2Height);
-	}//close left
-	
-	if (keyDown('D')) {
-		player2X = player2X + 5; //move right
-    image(player2, -player2X, player2Y, player2Width, player2Height);
-	}//close right
-
-  if (keyDown('S')) {
-    player2Y = player2Y + 5;
-  }
-
-  if (keyDown('W')) {
-    player2Y = player2Y - 5;
-  }
-
-  if (keyDown('Q')) {
-    jumpPlayer2 = true;
+function switchIdle(player) {
+  var playerSprite = player[0]
+  var lastMove = player[1]
+  if (lastMove == 'D'){
+    playerSprite.changeAnimation('idleD')
+  } else if (lastMove == 'U'){
+    playerSprite.changeAnimation('idleU')
   } else {
-    jumpPlayer2 = false;
-  }
+    playerSprite.changeAnimation('idleRL')
+  } 
+}
+
+function switchAttack(player) {
+  var playerSprite = player[0]
+  var lastMove = player[1]
+  if (lastMove == 'D'){
+    playerSprite.changeAnimation('attackD')
+  } else if (lastMove == 'U'){
+    playerSprite.changeAnimation('attackU')
+  } else {
+    playerSprite.changeAnimation('attackRL')
+  } 
+}
+
+function jump(player){
+  var playerSprite = player[0]
+  var lastMove = player[1]
+  var shadow = player[2]
+  playerSprite.scale = 2.3
+  shadow.scale = 0.45
+  if (lastMove == 'D'){
+    playerSprite.changeAnimation('walkD')
+    playerSprite.velocity.y = 3
+    shadow.velocity.y = 2
+  } else if (lastMove == 'U'){
+    playerSprite.changeAnimation('walkU')
+    playerSprite.velocity.y = -2
+    playerSprite.velocity.y = -3
+  } else {
+    playerSprite.changeAnimation('walkRL')
+    playerSprite.velocity.y = -1
+    if (lastMove == 'R'){
+      playerSprite.velocity.x = 2
+      shadow.velocity.x = 2
+    } else {
+      playerSprite.velocity.y = -2
+      shadow.velocity.x = -2
+    }
+  } 
 }
