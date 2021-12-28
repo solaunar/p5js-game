ad21032bd68e3dd5c10b36505e750f06ee38d38a
DebsTheLemon <depi.ge@hotmail.com>
2021-12-28 22:25:00 +0200

    uuuhhh

--- sketch.js
+++ sketch.js
@@ -8,8 +8,12 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  player1 = new Player(200, 200, 64, 64, 3, 5, 1.5, 'player1', ['W', 'S', 'A', 'D', 'Q', 'E']);
-  tile1 = new Tile("poop", [0, 0], 2);
+  player1 = new Player(200, 200, 64, 64, 2, 5, 1, 'player1', ['W', 'S', 'A', 'D', 'Q', 'E']);
+  tile1 = new Tile("poop", [1, 3], 3);
+  tile2 = new Tile("poop", [1, 1], 3);
+  tile3 = new Tile("poop", [5, 3], 3);
+  tile4 = new Tile("poop", [2, 0], 3);
+  tile5 = new Tile("poop", [5, 2], 3);
 }
 
 function windowResized() {
@@ -20,7 +24,49 @@ function windowResized() {
 
 function draw() {
   background(220);
-  tile1.draw(100, 100);
+
+  ///////////////////////////////// DRAWING MAP
+
+  let map =[2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 4, 5, 4, 4, 4, 4, 5, 4, 2,
+            2, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+            2, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+            2, 1, 1, 1, 3, 3, 1, 1, 1, 2,
+            2, 1, 1, 3, 1, 1, 3, 1, 1, 2,
+            2, 1, 1, 1, 3, 3, 1, 1, 1, 2,
+            2, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+            2, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2,]
+  
+  var col = 10;
+  var row = 10;
+  for (let i = 0; i < map.length; i++) {  
+    cory = Math.floor(i/row);
+    corx = i%col;
+
+    var scale = tile1.scale;
+    var size = scale*16;
+
+    switch(map[i]) {
+      case 2:
+        tile2.draw(100+corx*size, 100+cory*size);
+        break;
+      case 3:
+        tile3.draw(100+corx*size, 100+cory*size);
+        break;
+      case 4:
+        tile4.draw(100+corx*size, 100+cory*size);
+        break;
+      case 5:
+        tile5.draw(100+corx*size, 100+cory*size);
+        break;
+      default:
+        tile1.draw(100+corx*size, 100+cory*size);
+    }
+  }
+
+  ///////////////////////////////// DRAWING REST
+
   player1.draw();
   player1.update();
 }
