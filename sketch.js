c85a09263aa1dc177b9f9ac2c0013c3e19e47cf0
Ana <anna.lesi2000@gmail.com>
2021-12-28 14:41:01 +0200

    created player class

--- sketch.js
+++ sketch.js
@@ -1,160 +1,16 @@
-var player1 = [];
-var player2 = [];
-var imagesPath = 'assets/images/'
+var player1;
+var imagesPath = 'assets/images/';
 
 function preload() {
 }
 
 function setup() {
   createCanvas(800, 800);
-  player1[0] = initializePlayer(200, 200, 64, 64, 'player1')
-  player1[1] = 'D'
-  player1[2] = addShadow(player1)
-  //player2 = initializePlayer(300, 300, 64, 64, 'player2')
+  player1 = new Player(200, 200, 64, 54, 'player1', ['W', 'S', 'A', 'D', 'Q', 'E']);
 }
 
 function draw() {
   background(220);
-  drawSprite(player1[2])
-  drawSprite(player1[0])
-  playerMove(player1, 'W', 'S', 'A', 'D', 'Q', 'E')
-  //drawSprite(player2)
-  //playerMove(player2, 'UP_ARROW', 'DOWN_ARROW', 'LEFT_ARROW', 'RIGHT_ARROW', 'N', 'M', 'L')
-}
-
-function initializePlayer(x, y, height, width, playerName) {
-  var player = createSprite(x, y, height, width)
-  path = imagesPath + playerName
-  downPath = path + '/'
-  addAnimations(player, playerName, 'D', 'idleD', 1);
-  addAnimations(player, playerName, 'D', 'walkD', 6);
-  addAnimations(player, playerName, 'D', 'attackD', 9);
-  addAnimations(player, playerName, 'RL', 'idleRL', 1);
-  addAnimations(player, playerName, 'RL', 'walkRL', 6);
-  addAnimations(player, playerName, 'RL', 'attackRL', 9);
-  addAnimations(player, playerName, 'U', 'idleU', 1);
-  addAnimations(player, playerName, 'U', 'walkU', 6);
-  addAnimations(player, playerName, 'U', 'attackU', 9);
-  player.velocity.y = 0
-  return player
-}
-
-function addAnimations(playerSprite, playerName, direction, animationName, numberOfFrames){
-  path = imagesPath + `${playerName}/${direction}/${animationName}_`;
-  var framePaths = [];
-  for (let i = 0; i < numberOfFrames; i++) {
-    framePaths[i] = path + i + '.png'
-  }
-  animation = loadAnimation.apply(this, framePaths);
-  playerSprite.addAnimation(animationName, animation)
-}
-
-function addShadow(player) {
-  var shadow = createSprite(player[0].position.x, player[0].position.y + 38, 150, 160)
-  shadow.addImage(loadImage(`${imagesPath}/shadow.png`))
-  shadow.scale = 0.4
-  player[0].collide(shadow)
-  return shadow
-}
-
-function playerMove(player, moveUpKey, moveDownKey, moveLeftKey, moveRightKey, attackKey, jumpKey) {
-  var moveSpeed = 5
-  var playerSprite = player[0]
-  var shadow = player[2]
-
-  if (keyDown(jumpKey)) {
-    playerSprite.scale = 2.3
-    shadow.scale = 0.45
-    playerSprite.position.y = shadow.position.y - 58
-  } else {
-    playerSprite.scale = 2
-    shadow.scale = 0.4
-    playerSprite.position.y = shadow.position.y - 38
-  }
-
-  if (keyDown(moveUpKey)) {
-    playerSprite.changeAnimation('walkU')
-    playerSprite.position.y -= moveSpeed
-    shadow.position.y -= moveSpeed
-    player[1] = 'U'
-  }
-  else if (keyDown(moveDownKey)) {
-    playerSprite.changeAnimation('walkD')
-    playerSprite.position.y += moveSpeed
-    shadow.position.y += moveSpeed
-    player[1] = 'D'
-  }
-  else if (keyDown(moveLeftKey)) {
-    playerSprite.changeAnimation('walkRL')
-    playerSprite.mirrorX(-1)
-    playerSprite.position.x -= moveSpeed
-    shadow.mirrorX(-1)
-    shadow.position.x -= moveSpeed
-    player[1] = 'R'
-  }
-  else if (keyDown(moveRightKey)) {
-    playerSprite.changeAnimation('walkRL')
-    playerSprite.mirrorX(1)
-    playerSprite.position.x += moveSpeed
-    shadow.mirrorX(1)
-    shadow.position.x += moveSpeed
-    player[1] = 'L'
-  } else {
-    switchIdle(player)
-  }
-
-  if (keyDown(attackKey)) {
-    switchAttack(player)
-  }
-}
-
-function switchIdle(player) {
-  var playerSprite = player[0]
-  var lastMove = player[1]
-  if (lastMove == 'D'){
-    playerSprite.changeAnimation('idleD')
-  } else if (lastMove == 'U'){
-    playerSprite.changeAnimation('idleU')
-  } else {
-    playerSprite.changeAnimation('idleRL')
-  } 
-}
-
-function switchAttack(player) {
-  var playerSprite = player[0]
-  var lastMove = player[1]
-  if (lastMove == 'D'){
-    playerSprite.changeAnimation('attackD')
-  } else if (lastMove == 'U'){
-    playerSprite.changeAnimation('attackU')
-  } else {
-    playerSprite.changeAnimation('attackRL')
-  } 
-}
-
-function jump(player){
-  var playerSprite = player[0]
-  var lastMove = player[1]
-  var shadow = player[2]
-  playerSprite.scale = 2.3
-  shadow.scale = 0.45
-  if (lastMove == 'D'){
-    playerSprite.changeAnimation('walkD')
-    playerSprite.velocity.y = 3
-    shadow.velocity.y = 2
-  } else if (lastMove == 'U'){
-    playerSprite.changeAnimation('walkU')
-    playerSprite.velocity.y = -2
-    playerSprite.velocity.y = -3
-  } else {
-    playerSprite.changeAnimation('walkRL')
-    playerSprite.velocity.y = -1
-    if (lastMove == 'R'){
-      playerSprite.velocity.x = 2
-      shadow.velocity.x = 2
-    } else {
-      playerSprite.velocity.y = -2
-      shadow.velocity.x = -2
-    }
-  } 
+  player1.draw();
+  player1.update();
 }
---
+++ src/player.js
@@ -0,0 +1,163 @@
+class Player {
+    constructor(x, y, height, width, playerName, moveKeys) {
+        this.playerName = playerName;
+        this.lastMove = 'D';
+        this.lives = 3;
+        this.coins = 0;
+        this.moveKeys = moveKeys;
+        this.moveSpeed = 5;
+        this.sprite = createSprite(x, y, height, width);
+        this.shadow = this.addShadow();
+        this.dieMessage = "YOU DIED DUMBASS";
+        this.addAnimations();
+    }
+
+    draw(){
+        drawSprite(this.shadow);
+        drawSprite(this.sprite);
+    }
+
+    update(){
+        if (this.isAlive()){
+            this.playerMove();
+        } else {
+            textSize(50);
+            fill(255);
+            text(this.dieMessage, 250, 200);
+        }
+    }
+
+    addShadow() {
+        var shadow = createSprite(this.sprite.position.x, this.sprite.position.y + 40, 150, 160);
+        shadow.addImage(loadImage(`${imagesPath}/shadow.png`));
+        shadow.scale = 0.4;
+        this.sprite.collide(shadow);
+        return shadow;
+    }
+
+    addAnimations() {
+        this.addSingleAnimation('D', 'idleD', 1);
+        this.addSingleAnimation('D', 'walkD', 6);
+        this.addSingleAnimation('D', 'attackD', 9);
+        this.addSingleAnimation('RL', 'idleRL', 1);
+        this.addSingleAnimation('RL', 'walkRL', 6);
+        this.addSingleAnimation('RL', 'attackRL', 9);
+        this.addSingleAnimation('U', 'idleU', 1);
+        this.addSingleAnimation('U', 'walkU', 6);
+        this.addSingleAnimation('U', 'attackU', 9);
+    }
+
+    addSingleAnimation(direction, animationName, numberOfFrames) {
+        var path = './' + imagesPath + `${this.playerName}/${direction}/${animationName}_`;
+        var framePaths = [];
+        for (let i = 0; i < numberOfFrames; i++) {
+            framePaths[i] = path + i + '.png';
+        }
+        animation = loadAnimation.apply(this, framePaths);
+        this.sprite.addAnimation(animationName, animation);
+    }
+
+    isAlive() {
+        if (this.lives > 0)
+            return true;
+        return false;
+    }
+
+    switchIdle() {
+        if (this.lastMove == 'D') {
+            this.sprite.changeAnimation('idleD')
+        } else if (this.lastMove == 'U') {
+            this.sprite.changeAnimation('idleU')
+        } else {
+            this.sprite.changeAnimation('idleRL')
+        }
+    }
+
+    switchAttack() {
+        if (this.lastMove == 'D') {
+            this.sprite.changeAnimation('attackD')
+        } else if (this.lastMove == 'U') {
+            this.sprite.changeAnimation('attackU')
+        } else {
+            this.sprite.changeAnimation('attackRL')
+        }
+    }
+
+    jump() {
+        this.sprite.scale = 2.3
+        this.shadow.scale = 0.45
+        if (this.lastMove == 'D') {
+            this.sprite.changeAnimation('walkD')
+            this.sprite.velocity.y = 3
+            this.shadow.velocity.y = 2
+        } else if (this.lastMove == 'U') {
+            this.sprite.changeAnimation('walkU')
+            this.sprite.velocity.y = -2
+            this.sprite.velocity.y = -3
+        } else {
+            this.sprite.changeAnimation('walkRL')
+            this.sprite.velocity.y = -1
+            if (this.lastMove == 'R') {
+                this.sprite.velocity.x = 2
+                this.shadow.velocity.x = 2
+            } else {
+                this.sprite.velocity.y = -2
+                this.shadow.velocity.x = -2
+            }
+        }
+    }
+
+    playerMove() {
+        var moveUpKey = this.moveKeys[0];
+        var moveDownKey = this.moveKeys[1];
+        var moveLeftKey = this.moveKeys[2];
+        var moveRightKey = this.moveKeys[3];
+        var attackKey = this.moveKeys[4];
+        var jumpKey = this.moveKeys[5];
+
+        if (keyDown(jumpKey)) {
+            this.sprite.scale = 2.3
+            this.shadow.scale = 0.45
+            this.sprite.position.y = this.shadow.position.y - 58
+        } else {
+            this.sprite.scale = 2
+            this.shadow.scale = 0.4
+            this.sprite.position.y = this.shadow.position.y - 38
+        }
+
+        if (keyDown(moveUpKey)) {
+            this.sprite.changeAnimation('walkU')
+            this.sprite.position.y -= this.moveSpeed
+            this.shadow.position.y -= this.moveSpeed
+            this.lastMove = 'U'
+        }
+        else if (keyDown(moveDownKey)) {
+            this.sprite.changeAnimation('walkD')
+            this.sprite.position.y += this.moveSpeed
+            this.shadow.position.y += this.moveSpeed
+            this.lastMove = 'D'
+        }
+        else if (keyDown(moveLeftKey)) {
+            this.sprite.changeAnimation('walkRL')
+            this.sprite.mirrorX(-1)
+            this.sprite.position.x -= this.moveSpeed
+            this.shadow.mirrorX(-1)
+            this.shadow.position.x -= this.moveSpeed
+            this.lastMove = 'R'
+        }
+        else if (keyDown(moveRightKey)) {
+            this.sprite.changeAnimation('walkRL')
+            this.sprite.mirrorX(1)
+            this.sprite.position.x += this.moveSpeed
+            this.shadow.mirrorX(1)
+            this.shadow.position.x += this.moveSpeed
+            this.lastMove = 'L'
+        } else {
+            this.switchIdle()
+        }
+
+        if (keyDown(attackKey)) {
+            this.switchAttack()
+        }
+    }
+}
